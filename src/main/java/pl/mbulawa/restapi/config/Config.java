48634fe2d217f49574885a6b9e8ac0b5b2694d9b
Mateusz Bulawa <buleeeeek@gmail.com>
2022-01-31 23:00:23 +0100

    feat(config): add config class

---
+++ src/main/java/pl/mbulawa/restapi/config/Config.java
@@ -0,0 +1,18 @@
+package pl.mbulawa.restapi.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    void cutomizeObjectMapper(){
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+    }
+}
